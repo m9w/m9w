f67aa0352a01390e4f66d3c59ecd4d9858fe167e
m9w <52008918+m9w@users.noreply.github.com>
2023-11-27 12:17:16 +0200

    Totp app (#1)
    * Create index.html
    
    * Add files via upload
    
    * Create app.js
    
    * Add files via upload
    
    * Create bulma-0.9.4.min.css
    
    * Update index.html
    
    * Update app.js
    
    * Update index.html
    
    * Update index.html
    
    * Update index.html
    
    * Update index.html
    
    * Update index.html
    
    * Update index.html
    
    * Update index.html
    
    * Update app.js
    
    * Update index.html
    
    * Update app.js
    
    * Update app.js
    
    * Update app.js
    
    * Update app.js
    
    * Update app.js
    
    * Update app.js
    
    * Update index.html
    
    * Update app.js
    
    * Update index.html
    
    * Create README.md

---
+++ totp-app/js/app.js
@@ -0,0 +1,113 @@
+function getCurrentSeconds() {
+  return new Date().getTime() / 1000.0;
+}
+
+function stripSpaces(str) {
+  return str.replace(/\s/g, '');
+}
+
+function truncateTo(str, digits) {
+  if (str.length <= digits) {
+    return str;
+  }
+
+  return str.slice(-digits);
+}
+
+function parseURLSearch(search) {
+  const queryParams = search.substr(1).split('&').reduce(function (q, query) {
+    const chunks = query.split('=');
+    const key = chunks[0];
+    let value = decodeURIComponent(chunks[1]);
+    value = isNaN(Number(value)) ? value : Number(value);
+    return (q[key] = value, q);
+  }, {});
+
+  return queryParams;
+}
+
+const app = Vue.createApp({
+  data() {
+    return {
+      secret_key: 'JBSWY3DPEHPK3PXP',
+      digits: 6,
+      period: 30,
+      algorithm: 'SHA1',
+      updatingIn: 30,
+      token: null,
+      clipboardButton: null,
+    };
+  },
+
+  mounted: function () {
+    this.getKeyFromUrl();
+    this.getQueryParameters()
+    this.updateTimer();
+    this.updateCode();
+    this.intervalHandle = setInterval(this.updateTimer, 100);
+    this.clipboardButton = new ClipboardJS('#clipboard-button');
+  },
+
+  destroyed: function () {
+    clearInterval(this.intervalHandle);
+  },
+
+  computed: {
+    totp: function () {
+      return new OTPAuth.TOTP({
+        algorithm: this.algorithm,
+        digits: this.digits,
+        period: this.period,
+        secret: OTPAuth.Secret.fromBase32(stripSpaces(this.secret_key)),
+      });
+    }
+  },
+
+  methods: {
+    updateTimer: function () {
+      let updatingIn = this.period - (getCurrentSeconds() % this.period);
+      if(updatingIn > this.updatingIn) this.updateCode();
+      this.updatingIn = updatingIn;
+    },
+    
+    updateCode: function () {
+      this.token = truncateTo(this.totp.generate(), this.digits);
+      this.tokenValue = this.token;
+    },
+
+    copied: function () {
+      this.token = 'COPIED';
+      setTimeout(this.updateCode, 1000);
+    },
+
+    getKeyFromUrl: function () {
+      const key = document.location.hash.replace(/[#\/]+/, '');
+
+      if (key.length > 0) {
+        this.secret_key = key;
+      }
+    },
+    
+    getQueryParameters: function () {
+      const queryParams = parseURLSearch(window.location.search);
+
+      if (queryParams.key) {
+        this.secret_key = queryParams.key;
+      }
+
+      if (queryParams.digits) {
+        this.digits = queryParams.digits;
+      }
+
+      if (queryParams.period) {
+        this.period = queryParams.period;
+      }
+
+      if (queryParams.algorithm) {
+        this.algorithm = queryParams.algorithm;
+      }
+    }
+  }
+});
+
+app.mount('#app');
